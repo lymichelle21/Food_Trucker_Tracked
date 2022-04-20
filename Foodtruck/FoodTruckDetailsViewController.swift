dfc700021d6a384ed1e41e10d1d6f85c272b0f6e
Min Shin Khant <minshinkhant@Mins-MacBook-Pro.local>
2022-04-20 15:57:37 -0400

    adding map in the detail

--- Foodtruck/FoodTruckDetailsViewController.swift
+++ Foodtruck/FoodTruckDetailsViewController.swift
@@ -1,34 +1,41 @@
 import UIKit
 import AlamofireImage
+import MapKit
 
 class FoodtruckDetailsViewController: UIViewController {
 
     
-    @IBOutlet weak var backdropView: UIImageView!
-    @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var truckAddress: UILabel!
+    
+    @IBOutlet weak var truckMapLocation: MKMapView!
+    
     
     var foodtruck: [String:Any]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = foodtruck["title"] as? String
+        titleLabel.text = foodtruck["applicant"] as? String
         titleLabel.sizeToFit()
-        synopsisLabel.text = foodtruck["overview"] as? String
+        synopsisLabel.text = foodtruck["fooditems"] as? String
         
-        let baseUrl = "https://image.tmdb.org/t/p/w185"
-        let posterPath = foodtruck["poster_path"] as! String
-        let posterUrl = URL(string: baseUrl + posterPath)
-        posterView.af.setImage(withURL: posterUrl!)
+        //show the address
+        truckAddress.text = foodtruck["locationdescription"] as? String
+        // Set initial location
+        let truckLatitude = (foodtruck["latitude"] as! NSString).doubleValue
+        let truckLongitude = (foodtruck["longitude"] as! NSString).doubleValue
+        let initialLocation = CLLocation(latitude: truckLatitude, longitude: truckLongitude)
         
-        let backdropPath = foodtruck["backdrop_path"] as! String
-        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
-        backdropView.af.setImage(withURL: backdropUrl!)
+        truckMapLocation.centerToLocation(initialLocation)
         
+        //show the annotation of the truck on the map
+        let truck = MKPointAnnotation()
+        truck.title = foodtruck["applicant"] as? String
+        truck.coordinate = CLLocationCoordinate2D(latitude: truckLatitude, longitude: truckLongitude)
+        truckMapLocation.addAnnotation(truck)
 
-        // Do any additional setup after loading the view.
     }
     
 
@@ -42,4 +49,17 @@ class FoodtruckDetailsViewController: UIViewController {
     }
     */
 
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
 }
--- Foodtruck/FoodTruckViewController.swift
+++ Foodtruck/FoodTruckViewController.swift
@@ -12,28 +12,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         
         tableView.dataSource = self
-        tableView.delegate = self
-        
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-             // This will run when the network request returns
-             if let error = error {
-                    print(error.localizedDescription)
-             } else if let data = data {
-                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                    
-                 self.foodtrucks = dataDictionary["results"] as! [[String:Any]]
-                 self.tableView.reloadData()
-                    // TODO: Get the array of foodtrucks
-                    // TODO: Store the foodtrucks in a property to use elsewhere
-                    // TODO: Reload your table view data
-                 print(dataDictionary)
+                tableView.delegate = self
+                
+                let url = URL(string: "https://data.sfgov.org/resource/rqzj-sfat.json")!
+                let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+                let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+                let task = session.dataTask(with: request) { (data, response, error) in
+                     // This will run when the network request returns
+                     if let error = error {
+                            print(error.localizedDescription)
+                     } else if let data = data {
+                         self.foodtrucks = try! JSONSerialization.jsonObject(with: data, options: []) as! [[String: Any]]
+                            
+                         
+                         self.tableView.reloadData()
+                            // TODO: Get the array of foodtrucks
+                            // TODO: Store the foodtrucks in a property to use elsewhere
+                            // TODO: Reload your table view data
+                         
+                         //print(foodtrucks)
 
-             }
-        }
-        task.resume()
+                     }
+                }
+                task.resume()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -41,44 +42,44 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FoodtruckCell") as! FoodtruckCell
-        let foodtruck = foodtrucks[indexPath.row]
-        let title = foodtruck["title"] as! String
-        let synopsis = foodtruck["overview"] as! String
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FoodtruckCell") as! FoodtruckCell
+            let foodtruck = foodtrucks[indexPath.row]
+            let title = foodtruck["applicant"] as! String
+            let synopsis = foodtruck["fooditems"] as! String
+            
+            cell.titleLabel.text = title
+            cell.synopsisLabel.text = synopsis
+            
+            //let baseUrl = "https://image.tmdb.org/t/p/w185"
+            //let posterPath = foodtruck["poster_path"] as! String
+            //let posterUrl = URL(string: baseUrl + posterPath)
+            //cell.posterView.af.setImage(withURL: posterUrl!)
+            
+            return cell
+        }
         
-        cell.titleLabel.text = title
-        cell.synopsisLabel.text = synopsis
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+        {
+            print("Loading up details screen")
+            let cell = sender as! UITableViewCell
+            let indexPath = tableView.indexPath(for: cell)!
+            let foodtruck = foodtrucks[indexPath.row]
+            
+            // Pass selected foodtrucks to details view controller
+            let detailsViewController = segue.destination as!FoodtruckDetailsViewController
+            detailsViewController.foodtruck = foodtruck
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
         
-        let baseUrl = "https://image.tmdb.org/t/p/w185"
-        let posterPath = foodtruck["poster_path"] as! String
-        let posterUrl = URL(string: baseUrl + posterPath)
-        cell.posterView.af.setImage(withURL: posterUrl!)
-        
-        return cell
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
-        print("Loading up details screen")
-        let cell = sender as! UITableViewCell
-        let indexPath = tableView.indexPath(for: cell)!
-        let foodtruck = foodtrucks[indexPath.row]
-        
-        // Pass selected foodtrucks to details view controller
-        let detailsViewController = segue.destination as!FoodtruckDetailsViewController
-        detailsViewController.foodtruck = foodtruck
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    @IBAction func onLogoutButton(_ sender: Any) {
-        PFUser.logOut()
-        let main = UIStoryboard(name: "Main", bundle: nil)
-        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
-        
-        delegate.window?.rootViewController = loginViewController
-    }
+        @IBAction func onLogoutButton(_ sender: Any) {
+            PFUser.logOut()
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+            
+            delegate.window?.rootViewController = loginViewController
+        }
 
 }
 
